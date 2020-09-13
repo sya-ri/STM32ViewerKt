47b42402394ec40b5b73823260b4b689db097e06
sya-ri <sya79lua@gmail.com>
2020-09-14 00:51:48 +0900

    MainViewにメニューを追加

--- src/main/kotlin/me/syari/stm32/viewer/ui/MainView.kt
+++ src/main/kotlin/me/syari/stm32/viewer/ui/MainView.kt
@@ -1,8 +1,33 @@
 package me.syari.stm32.viewer.ui
 
+import javafx.fxml.FXML
 import javafx.scene.Parent
+import javafx.scene.control.MenuItem
+import javafx.stage.FileChooser
 import tornadofx.View
+import tornadofx.chooseFile
 
 class MainView : View("STM32ViewerKt") {
     override val root: Parent by fxml("/fxml/MainView.fxml")
+
+    @FXML lateinit var menuItemRun: MenuItem
+
+    fun clickMenuOpenElf() {
+        chooseFile(null, arrayOf(FileChooser.ExtensionFilter("Executable File", "elf")))
+    }
+
+    fun clickMenuOpenPluginConfig() {
+        openInternalWindow(PluginOptionView::class)
+    }
+
+    var isRunning = false
+
+    fun clickMenuRun() {
+        isRunning = isRunning.not()
+        menuItemRun.text = if (isRunning) {
+            "Stop"
+        } else {
+            "Run"
+        }
+    }
 }
