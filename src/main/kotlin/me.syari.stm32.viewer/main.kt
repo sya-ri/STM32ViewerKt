fd791d3e945641f18c2f069cac7ad99fe563fa12
sya-ri <sya79lua@gmail.com>
2020-09-13 12:19:13 +0900

    不要なアノテーションを削除

--- src/main/kotlin/me.syari.stm32.viewer/main.kt
+++ src/main/kotlin/me.syari.stm32.viewer/main.kt
@@ -1,6 +1,5 @@
 package me.syari.stm32.viewer
 
-@ExperimentalStdlibApi
 fun main(){
     val plugins = Plugins.find("/Applications/STM32CubeIDE.app/Contents/Eclipse/plugins")
         ?: return println("Not Found: Plugins Folder")
