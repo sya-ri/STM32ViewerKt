7efac403c46e03a8581fb3260b4555beae7c52f9
sya-ri <sya79lua@gmail.com>
2020-09-13 22:18:31 +0900

    build.gradle.kts のフォーマットを修正

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.4.10"
 }
+
 group = "me.syari.stm32.viewer"
 version = "1.0"
 
@@ -14,6 +15,6 @@ dependencies {
     implementation("no.tornado:tornadofx:1.7.20")
 }
 
-tasks.withType<KotlinCompile>() {
+tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
