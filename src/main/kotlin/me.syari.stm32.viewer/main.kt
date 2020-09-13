18dc67d8a23fb49c6bd3b99efbc595d99e1213a9
sya-ri <sya79lua@gmail.com>
2020-09-13 12:16:44 +0900

    ST-LINK_gdbserverでポートを開くことに成功

--- src/main/kotlin/me.syari.stm32.viewer/main.kt
+++ src/main/kotlin/me.syari.stm32.viewer/main.kt
@@ -8,9 +8,10 @@ fun main(){
         println("${it.key} ${it.value}")
     }
     val stLinkGdbServer = plugins[Plugin.StLinkGdbServer] ?: return println("Not Found: ST-Link GDB Server")
+    val cubeProgrammer = plugins[Plugin.CubeProgrammer] ?: return println("Not Found: CubeProgrammer")
     ProcessBuilder().apply {
         directory(stLinkGdbServer.toolsBin)
         redirectOutput(ProcessBuilder.Redirect.INHERIT)
-        command("./ST-LINK_gdbserver")
+        command("./ST-LINK_gdbserver", "-d", "-v", "-cp", "'${cubeProgrammer.toolsBin?.absolutePath}'")
     }.start()
 }
