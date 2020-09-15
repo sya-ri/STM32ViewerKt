f7ec69970793b2e4045453d01a668def6632ba7f
sya-ri <sya79lua@gmail.com>
2020-09-16 08:56:01 +0900

    elfが選択できないバグの修正 実行できるかのテストをした

--- src/main/kotlin/me/syari/stm32/viewer/ui/MainView.kt
+++ src/main/kotlin/me/syari/stm32/viewer/ui/MainView.kt
@@ -4,6 +4,9 @@ import javafx.fxml.FXML
 import javafx.scene.Parent
 import javafx.scene.control.MenuItem
 import javafx.stage.FileChooser
+import me.syari.stm32.viewer.config.Config
+import me.syari.stm32.viewer.launchArmNoneEabiGdb
+import me.syari.stm32.viewer.launchStLinkGdbServer
 import tornadofx.View
 import tornadofx.chooseFile
 
@@ -13,7 +16,7 @@ class MainView : View("STM32ViewerKt") {
     @FXML lateinit var menuItemRun: MenuItem
 
     fun clickMenuOpenElf() {
-        chooseFile(null, arrayOf(FileChooser.ExtensionFilter("Executable File", "elf")))
+        chooseFile(null, arrayOf(FileChooser.ExtensionFilter("Executable File", "*.elf")))
     }
 
     fun clickMenuOpenPluginOption() {
@@ -25,6 +28,8 @@ class MainView : View("STM32ViewerKt") {
     fun clickMenuRun() {
         isRunning = isRunning.not()
         menuItemRun.text = if (isRunning) {
+            launchStLinkGdbServer(Config.Plugin.STLinkGDBServer.get()!!, Config.Plugin.CubeProgrammer.get()!!)
+            launchArmNoneEabiGdb(Config.Plugin.GnuArmEmbedded.get()!!)
             "Stop"
         } else {
             "Run"
